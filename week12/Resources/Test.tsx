c3cfa6ae54bf1fd52f83ea3143bf668b824f0e57
yaoxh6 <2879813862@qq.com>
2018-06-02 23:04:44 +0800

    week12瓦片

---
+++ week12/Resources/Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Test" tilewidth="30" tileheight="30" spacing="1" margin="1" tilecount="294" columns="21">
+ <image source="Test.jpg" width="658" height="454"/>
+</tileset>
